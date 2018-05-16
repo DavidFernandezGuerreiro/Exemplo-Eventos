98c44e11650190e9494daa88d52b5f692ca5f945
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-05-16 09:04:09 +0200

    Commit git

---
+++ src/exemploeventos/ExemploEventos.java
@@ -0,0 +1,17 @@
+
+package exemploeventos;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class ExemploEventos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Evento obxEvento=new Evento();
+    }
+    
+}
